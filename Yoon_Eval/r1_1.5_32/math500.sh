adaa67d4519db2dc895d513e51662c7beea0bf64
Yoongon-Kim <yooongonkim@gmail.com>
2025-12-30 16:28:47 +0000

    math500 shell file added

---
+++ Yoon_Eval/r1_1.5_32/math500.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the evaluation command four times
+# Each run automatically creates a separate output directory with a timestamp
+for i in {1..4}
+do
+    echo "Running iteration $i of 4..."
+    python script/evaluate/hf_dataset_sglang.py --dataset math500 --router_path resource/r1_1.5_32_router.pt --use_hybrid
+    echo "Completed iteration $i"
+    echo "----------------------------------------"
+done
+
+echo "All 4 iterations completed!"
