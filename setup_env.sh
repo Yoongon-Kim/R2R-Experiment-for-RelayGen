b939910b12754e4e6c372eebdcf50a82fb7351cf
Yoongon-Kim <yooongonkim@gmail.com>
2025-12-30 15:46:24 +0000

    math dataset covered

--- setup_env.sh
+++ setup_env.sh
@@ -4,3 +4,4 @@ pip install setproctitle
 pip install sglang==0.4.6
 pip install sgl-kernel==0.1.0
 pip install flashinfer-python==0.2.3 -i https://flashinfer.ai/whl/cu124/torch2.6/
+pip install datasets latex2sympy2
